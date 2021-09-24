9210d501e6b9197e2947c1c0adbf2604316b40c6
giofile <monsanto.g@chello.nl>
2021-09-24 21:28:01 +0200

    saver

--- songsaver/src/About.js
+++ songsaver/src/About.js
@@ -4,6 +4,7 @@ const About = () => {
     return (
           <div>
                  <h3>About SongSaver App </h3>
+                 <br/>
                         <h5>Met de SongSaver App heeft u de mogelijkheid om u leukste liedjes op te slaan,
                             <br/> zodat u ze 
                         niet kwijtraakt. U kunt een <br />waardering aangeven en tevens kunt ook liedjes verwijderen
